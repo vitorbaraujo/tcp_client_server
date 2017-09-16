9f16853387003c169c738cfd93257b1c1d3b83d9
Vitor Barbosa <vitornga15@gmail.com>
2017-09-16 18:00:40 -0300

    Finish client server packet sending

--- c/client.c
+++ c/client.c
@@ -8,15 +8,26 @@
 #include <arpa/inet.h>
 
 typedef struct req {
-  char op[3];
-  int op1;
-  int op2;
+    char op[3];
+    int op1;
+    int op2;
 } req_pkt;
 
 typedef struct answ {
-  int invalid;
-  int value;
+    int invalid;
+    int value;
 } answ_pkt;
+
+char* print_error(int code){
+    switch(code){
+        case 0:
+            printf("Division by zero\n");
+            break;
+        case -1:
+            printf("Operation is invalid\n");
+            break;
+    }
+}
 
 char *parse_op(char op){
     if(op == '+') return "add";
@@ -28,12 +39,17 @@ char *parse_op(char op){
 }
 
 req_pkt *parse_msg(char *input){
-    char first_n[512], second_n[512], op;
+    char first_n[512], second_n[512], op, *delimiter = "\n";
     int frst_num_ptr = 0, sec_num_ptr = 0;
     req_pkt *requisition = malloc(sizeof(req_pkt));
 
     int op_found = 0;
     int input_size = strlen(input);
+
+    if(strcmp(input, "close\n") == 0){
+        strcpy(requisition->op, "cls");
+        return requisition;
+    }
 
     for(int i=0; i<input_size; i++){
         if(isspace(input[i])) continue;
@@ -72,47 +88,58 @@ req_pkt *parse_msg(char *input){
 }
 
 int main(int argc, char* argv[]){
-  if(argc < 3){
-    printf("Specify port\n");
-    exit(1);
-  }
+    if(argc < 3){
+        printf("Specify port\n");
+        exit(1);
+    }
 
-  struct sockaddr_in client_address, server_address;
-  int server, read_v;
-  char operacao[512] = {0};
+    struct sockaddr_in client_address, server_address;
+    int server, read_v;
+    char operacao[512] = {0};
 
-  if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    printf("Socket error\n");
-    exit(1);
-  }
+    if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("Socket error\n");
+        exit(1);
+    }
 
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(atoi(argv[2]));
-  server_address.sin_addr.s_addr = inet_addr(argv[1]);
-  memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(argv[2]));
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+    memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
 
-  if(connect(server, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
-    printf("Error on connect\n");
-    exit(1);
-  }
+    if(connect(server, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
+        printf("Error on connect\n");
+        exit(1);
+    }
 
-  printf("Digite as operacoes:\n");
-  req_pkt *data_to_send = malloc(sizeof(req_pkt));
-  answ_pkt *result = malloc(sizeof(answ_pkt));
+    printf("Client connected with server on IP [%s] and port [%s]\n", argv[1], argv[2]);
 
-  while(1){
-      fgets(operacao, 512, stdin);
-      data_to_send = parse_msg(operacao);
-      send(server, data_to_send, sizeof(req_pkt), 0);
-      do{
-        read_v = read(server, result, sizeof(answ_pkt));
-      }while(read_v == 0);
-      if(result->invalid == 0){
-        printf("Resultado: %d\n", result->value);
-      }else{
-        printf("Houve um erro no pedido de calculo\n");
-      }
-  }
+    printf("Insert operation: ");
+    req_pkt *data = malloc(sizeof(req_pkt));
+    answ_pkt *result = malloc(sizeof(answ_pkt));
 
-  return 0;
+    while(1){
+        fgets(operacao, 512, stdin);
+        data = parse_msg(operacao);
+
+        printf("Sending packet [%s %d %d] to server... ", data->op, data->op1, data->op2);
+        send(server, data, sizeof(req_pkt), 0);
+        printf("Sent!\n");
+
+        do{
+            read_v = read(server, result, sizeof(answ_pkt));
+        }while(read_v == 0);
+
+        printf("\nPacket received from server: [%d %d]\n", result->invalid, result->value);
+
+        if(!result->invalid){
+            printf("Result: [%d]\n", result->value);
+        }
+        else{
+            printf("INVALID: ");
+            print_error(result->value);
+        }
+    }
+
+    return 0;
 }
--- c/server.c
+++ c/server.c
@@ -7,111 +7,140 @@
 #include <arpa/inet.h>
 
 typedef struct req {
-  char op[3];
-  int op1;
-  int op2;
+    char op[3];
+    int op1;
+    int op2;
 } req_pkt;
 
 typedef struct answ {
-  int invalid;
-  int value;
+    int invalid;
+    int value;
 } answ_pkt;
 
 answ_pkt *add(int a, int b){
-  answ_pkt *result = malloc(sizeof(answ_pkt));
-  result->invalid = 0;
-  result->value = a+b;
-  return result;
+    answ_pkt *result = malloc(sizeof(answ_pkt));
+    result->invalid = 0;
+    result->value = a+b;
+
+    return result;
 }
 
 answ_pkt *sub(int a, int b){
-  answ_pkt *result = malloc(sizeof(answ_pkt));
-  result->invalid = 0;
-  result->value = a-b;
-  return result;
+    answ_pkt *result = malloc(sizeof(answ_pkt));
+    result->invalid = 0;
+    result->value = a-b;
+
+    return result;
 }
 
 answ_pkt *mul(int a, int b){
-  answ_pkt *result = malloc(sizeof(answ_pkt));
-  result->invalid = 0;
-  result->value = a*b;
-  return result;
+    answ_pkt *result = malloc(sizeof(answ_pkt));
+    result->invalid = 0;
+    result->value = a*b;
+
+    return result;
 }
 
 answ_pkt *divi(int a, int b){
-  answ_pkt* result = malloc(sizeof(answ_pkt));
-  if(!b){
-    result->invalid = 1;
-    return result;
-  }
+    answ_pkt* result = malloc(sizeof(answ_pkt));
+    if(!b){
+        result->invalid = 1;
+        return result;
+    }
 
-  result->invalid = 0;
-  result->value = a/b;
-  return result;
+    result->invalid = 0;
+    result->value = a/b;
+
+    return result;
 }
 
 answ_pkt *calc(req_pkt *requisition){
-  answ_pkt *answer = malloc(sizeof(answ_pkt));
-  if(!(strcmp(requisition->op, "add"))){
-    answer = add(requisition->op1, requisition->op2);
-  }else if(!(strcmp(requisition->op, "sub"))){
-    answer = sub(requisition->op1, requisition->op2);
-  }else if(!(strcmp(requisition->op, "mul"))){
-    answer = mul(requisition->op1, requisition->op2);
-  }else if(!(strcmp(requisition->op, "div"))){
-    answer = divi(requisition->op1, requisition->op2);
-  }else{
-    answer->invalid = 1;
-  }
+    answ_pkt *answer = malloc(sizeof(answ_pkt));
+    if(!(strcmp(requisition->op, "add"))){
+        answer = add(requisition->op1, requisition->op2);
+    }
+    else if(!(strcmp(requisition->op, "sub"))){
+        answer = sub(requisition->op1, requisition->op2);
+    }
+    else if(!(strcmp(requisition->op, "mul"))){
+        answer = mul(requisition->op1, requisition->op2);
+    }
+    else if(!(strcmp(requisition->op, "div"))){
+        answer = divi(requisition->op1, requisition->op2);
+    }
+    else if(!(strcmp(requisition->op, "cls"))){
+        answer->invalid = 1000;
+    }
+    else{
+        answer->invalid = 1;
+        answer->value = -1;
+    }
 
-  return answer;
+    return answer;
 }
 
 int main(int argc, char* argv[]){
-  if(argc < 3){
-    printf("Specify port\n");
-    exit(1);
-  }
+    if(argc < 3){
+        printf("Specify port\n");
+        exit(1);
+    }
 
-  req_pkt *buffer = malloc(sizeof(req_pkt));
-  struct sockaddr_in server_address;
-  int server, client, read_v, addr_length = sizeof(server_address);
+    req_pkt *buffer = malloc(sizeof(req_pkt));
+    struct sockaddr_in server_address, connection_address;
+    int server, client, read_v, addr_length = sizeof(server_address), connection_length = sizeof(connection_address);
 
-  if((server = socket(AF_INET, SOCK_STREAM, 0)) == 0){
-    printf("Error on socket\n");
-    exit(1);
-  }
+    if((server = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        printf("Error on socket\n");
+        exit(1);
+    }
 
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(atoi(argv[2]));
-  server_address.sin_addr.s_addr = inet_addr(argv[1]);
-  memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(argv[2]));
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+    memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
 
-  if(bind(server, (struct sockaddr *) &server_address, sizeof(server_address))){
-    printf("Error on bind\n");
-    exit(1);
-  }
+    if(bind(server, (struct sockaddr *) &server_address, sizeof(server_address))){
+        printf("Error on bind\n");
+        exit(1);
+    }
 
-  if(listen(server, 1) < 0){
-    printf("Error on listening\n");
-    exit(1);
-  }
+    if(listen(server, 1) < 0){
+        printf("Error on listening\n");
+        exit(1);
+    }
 
-  if((client = accept(server, (struct sockaddr *)&server_address, (socklen_t*) &addr_length)) < 0){
-    printf("Error accepting\n");
-    exit(1);
-  }
+    printf("Waiting for client to connect...\n");
 
-  answ_pkt* result = malloc(sizeof(answ_pkt));
+    if((client = accept(server, (struct sockaddr *)&connection_address, (socklen_t*) &connection_length)) < 0){
+        printf("Error accepting\n");
+        exit(1);
+    }
 
-  while(1){
-    do{
-      read_v = read(client, buffer, sizeof(req_pkt));
-    }while(read_v == 0);
+    printf("Client with IP [%s] and port [%d] has connected!\n", inet_ntoa(connection_address.sin_addr), ntohs(connection_address.sin_port));
 
-    result = calc(buffer);
-    send(client, result, sizeof(answ_pkt), 0);
-  }
+    answ_pkt* result = malloc(sizeof(answ_pkt));
 
-  return 0;
+    while(1){
+        do{
+            read_v = read(client, buffer, sizeof(req_pkt));
+        }while(read_v == 0);
+
+        printf("Package received!\n");
+        printf("Content: [%s %d %d]\n", buffer->op, buffer->op1, buffer->op2);
+
+        if(strcmp(buffer->op, "cls") == 0){
+            close(client);
+            exit(0);
+        }
+
+        printf("Calculating...\n\n");
+
+        result = calc(buffer);
+
+        printf("Sending answer to client... ");
+        send(client, result, sizeof(answ_pkt), 0);
+        printf("Sent!\n\n");
+    }
+
+    return 0;
 }
