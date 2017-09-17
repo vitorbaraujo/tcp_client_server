43b07ccd263be03644c8919d35b91c9b1b23ba8b
Vitor Barbosa <vitornga15@gmail.com>
2017-09-17 11:38:05 -0300

    Add comments on functions

--- c/client.c
+++ c/client.c
@@ -7,17 +7,27 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+// struct that contains request packet
+// parameters:
+//  op[3]: operator
+//  op1: first member of operation
+//  op2: second member of operation
 typedef struct req {
     char op[3];
     int op1;
     int op2;
 } req_pkt;
 
+// struct that contains answer packet
+// parameters:
+//  invalid: flag to tell the client the answer is invalid
+//  value: result (if packet is valid) or error code (otherwise)
 typedef struct answ {
     int invalid;
     int value;
 } answ_pkt;
 
+// receives an error code and returns the corresponding error message
 char* print_error(int code){
     switch(code){
         case 0:
@@ -29,6 +39,8 @@ char* print_error(int code){
     }
 }
 
+// receives an operator and returns the corresponding string
+// if operation is invalid returns "inv"
 char *parse_op(char op){
     if(op == '+') return "add";
     else if(op == '-') return "sub";
@@ -38,6 +50,16 @@ char *parse_op(char op){
     return "inv";
 }
 
+// parse client request
+// if input is valid:
+//  input: "2 + 3"
+//  output: "add 2 3"
+// if input is "close"
+//  input: "close"
+//  output: "cls"
+// if input is invalid
+//  input: "2 a 3"
+//  output: "inv 0 0"
 req_pkt *parse_msg(char *input){
     char first_n[512], second_n[512], op, *delimiter = "\n";
     int frst_num_ptr = 0, sec_num_ptr = 0;
@@ -97,16 +119,19 @@ int main(int argc, char* argv[]){
     int server, read_v;
     char operacao[512] = {0};
 
+    // creates TCP socket
     if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         printf("Socket error\n");
         exit(1);
     }
 
+    // insert family, port and IP to server socket that is going to connect
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(atoi(argv[2]));
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
     memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
 
+    // connects to the specified server described in server_address
     if(connect(server, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
         printf("Error on connect\n");
         exit(1);
@@ -122,11 +147,13 @@ int main(int argc, char* argv[]){
         fgets(operacao, 512, stdin);
         data = parse_msg(operacao);
 
+        // send request packet to server
         printf("Sending packet [%s %d %d] to server... ", data->op, data->op1, data->op2);
         send(server, data, sizeof(req_pkt), 0);
         printf("Sent!\n");
 
         do{
+            // receives answer packet from server
             read_v = read(server, result, sizeof(answ_pkt));
         }while(read_v == 0);
 
--- c/server.c
+++ c/server.c
@@ -6,17 +6,30 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+// struct that contains request packet
+// parameters:
+//  op[3]: operator
+//  op1: first member of operation
+//  op2: second member of operation
 typedef struct req {
     char op[3];
     int op1;
     int op2;
 } req_pkt;
 
+// struct that contains answer packet
+// parameters:
+//  invalid: flag to tell the client the answer is invalid
+//  value: result (if packet is valid) or error code (otherwise)
 typedef struct answ {
     int invalid;
     int value;
 } answ_pkt;
 
+// function that adds two numbers
+// parameters:
+//  a: first number
+//  b: second number
 answ_pkt *add(int a, int b){
     answ_pkt *result = malloc(sizeof(answ_pkt));
     result->invalid = 0;
@@ -25,6 +38,10 @@ answ_pkt *add(int a, int b){
     return result;
 }
 
+// function that subtract two numbers
+// parameters:
+//  a: first number
+//  b: second number
 answ_pkt *sub(int a, int b){
     answ_pkt *result = malloc(sizeof(answ_pkt));
     result->invalid = 0;
@@ -33,6 +50,10 @@ answ_pkt *sub(int a, int b){
     return result;
 }
 
+// function that multiplies two numbers
+// parameters:
+//  a: first number
+//  b: second number
 answ_pkt *mul(int a, int b){
     answ_pkt *result = malloc(sizeof(answ_pkt));
     result->invalid = 0;
@@ -41,6 +62,11 @@ answ_pkt *mul(int a, int b){
     return result;
 }
 
+// function that divides two numbers
+// parameters:
+//  a: first number
+//  b: second number
+// NOTE: if there is a division by zero, the flag invalid is set to 1
 answ_pkt *divi(int a, int b){
     answ_pkt* result = malloc(sizeof(answ_pkt));
     if(!b){
@@ -54,6 +80,9 @@ answ_pkt *divi(int a, int b){
     return result;
 }
 
+// function that receives a request packet,
+//  calculates the answer (if the request is valid)
+//  and mount the answer packet with proper values
 answ_pkt *calc(req_pkt *requisition){
     answ_pkt *answer = malloc(sizeof(answ_pkt));
     if(!(strcmp(requisition->op, "add"))){
@@ -87,23 +116,27 @@ int main(int argc, char* argv[]){
 
     req_pkt *buffer = malloc(sizeof(req_pkt));
     struct sockaddr_in server_address, connection_address;
-    int server, client, read_v, addr_length = sizeof(server_address), connection_length = sizeof(connection_address);
+    int server, client, read_v;
+    int addr_length = sizeof(server_address), connection_length = sizeof(connection_address);
 
     if((server = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         printf("Error on socket\n");
         exit(1);
     }
 
+    // insert family, port and IP to server socket
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(atoi(argv[2]));
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
     memset(server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
 
+    // bind the socket with the information passed on server_address
     if(bind(server, (struct sockaddr *) &server_address, sizeof(server_address))){
         printf("Error on bind\n");
         exit(1);
     }
 
+    // make socket listen on that port specified on server_address
     if(listen(server, 1) < 0){
         printf("Error on listening\n");
         exit(1);
@@ -111,17 +144,20 @@ int main(int argc, char* argv[]){
 
     printf("Waiting for client to connect...\n");
 
+    // when client connects to the server port, the server accepts the connection
+    //  and creates a new socket to be used in the connection
     if((client = accept(server, (struct sockaddr *)&connection_address, (socklen_t*) &connection_length)) < 0){
         printf("Error accepting\n");
         exit(1);
     }
 
-    printf("Client with IP [%s] and port [%d] has connected!\n", inet_ntoa(connection_address.sin_addr), ntohs(connection_address.sin_port));
+    printf("Client with IP [%s] and port [%d] has connected!\n", inet_ntoa(connection_address.sin_addr),        ntohs(connection_address.sin_port));
 
     answ_pkt* result = malloc(sizeof(answ_pkt));
 
     while(1){
         do{
+            // read information from client
             read_v = read(client, buffer, sizeof(req_pkt));
         }while(read_v == 0);
 
@@ -137,6 +173,7 @@ int main(int argc, char* argv[]){
 
         result = calc(buffer);
 
+        // send information to client
         printf("Sending answer to client... ");
         send(client, result, sizeof(answ_pkt), 0);
         printf("Sent!\n\n");
